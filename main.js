d8a5c316464dd5999e79175af9fe1671c5caec72
Sriharini <sriharini01122004@gmail.com>
2024-11-11 21:28:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('._nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header'): header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll',shadowHeader)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+              if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-Link')
+              }else{
+                sectionsClass.classList.remove('active-Link')
+              }
+           
+    })
+}
+window.addEventListener('scroll',scrollActive)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    //reset: true, //Animation repeat
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__dish`, {delay: 500, distance: '100px', origin: 'bottom'})
+sr.reveal(`.home__burger`, {delay: 1200, distance: '100px', duration: 1500})
+sr.reveal(`.home__ingredient`, {delay: 1600, interval:100})
+sr.reveal(`.recipe__img, delivery__img, .contact__image`, {origin:left})
+sr.reveal(`.recipe__data, delivery__data, .contact__data `, {origin: left})
+sr.reveal(`.popular__card`, {interval:100})
+
+
